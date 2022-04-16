3b6e85f34be8d0e1279b8f59a54746bda3d68cb9
spacefrogo <valton.gara@gmail.com>
2022-04-17 00:04:14 +0200

    update

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ const skills_bars = document.querySelectorAll(".skill-progress");
 const records_wrap = document.querySelector(".records");
 const records_number = document.querySelectorAll(".number");
 const footer_input = document.querySelector(".footer-input");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
 
 filter_btns.forEach((btn) => {
@@ -72,3 +75,28 @@ function countUp() {
       setTimeout(updateCount, 800);
     }); 
 }
+
+footer_input.addEventListener("focus", () => {
+  footer_input.classList.add("focus");
+});
+
+footer_input.addEventListener("blur", () => {
+  if (footer_input.value != "") return;
+  footer_input.classList.remove("focus");
+});
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
