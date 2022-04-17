5ac436cdd24713eedd6b806d2ee4ebf4d454a8d9
spacefrogo <valton.gara@gmail.com>
2022-04-17 11:45:29 +0200

    update

--- js/app.js
+++ js/app.js
@@ -31,17 +31,6 @@ window.addEventListener("scroll", () => {
   countUp();
 })
 
-footer_input.addEventListener("focus", () =>{
-  footer_input.classList.add("focus");
-}
-)
-
-footer_input.addEventListener("blur", () =>{
-  if (footer_input.value != "") return;
-  footer_input.classList.remove("focus");
-}
-)
-
 
 function checkScroll(el) {
     let rect = el.getBoundingClientRect();
